f89d8dd8a19e92ae0fff7293f7a5931509e2fed3
Corey Michaud <cor3y.michaud@gmail.com>
2025-12-04 07:49:55 -0500

    Adding template for part 2 to edit later

--- day_01.py
+++ day_01.py
@@ -1,34 +1,57 @@
-# Use to switch between parts so you don't have to comment/uncomment code
-PART = 1
-
 # Part 1
-if PART == 1:
 
-    # Combination order
-    with open("inputs/day_01_input_01.txt", "r") as f:
-        combination_order = [line.strip() for line in f]
+# Order of inputs
+with open("inputs/day_01_input.txt", "r") as f:
+    combination_order = [line.strip() for line in f]
 
-    # Function to count the number of times the combination landed on zero
-    def count_zeros(input_list: list) -> int:
+# Function to count the number of times the combination landed on zero
+def count_zeros(input_list: list) -> int:
 
-        counter = 0
-        index = 50
-        step = 0
+    counter = 0
+    index = 50
+    step = 0
 
-        for item in input_list:
+    for item in input_list:
 
-            if item.startswith("L"):
-                step = int(item[1:])
-                index = index - step
-            else:
-                step = int(item[1:])
-                index = index + step
+        step = int(item[1:])
 
-            if index % 100 == 0:
-                counter += 1
+        if item.startswith("L"):
+            index = index - step
+        else:
+            index = index + step
 
-        return counter
+        if index % 100 == 0:
+            counter += 1
 
-    print(count_zeros(combination_order)) # The answer is 980
+    return counter
+
+# Answer
+answer_1 = count_zeros(combination_order)
+print(f"The answer to part 1 is: {answer_1}")
 
 # Part 2
+
+# This time adding functionality to also count if dial passed through zero
+def count_zeros_and_between(input_list: list) -> int:
+
+    counter = 0
+    index = 50
+    step = 0
+
+    for item in input_list:
+
+        step = int(item[1:])
+
+        if item.startswith("L"):
+            index = index - step
+        else:
+            index = index + step
+
+        if index % 100 == 0:
+            counter += 1
+
+    return counter
+
+# Answer
+answer_2 = count_zeros_and_between(combination_order)
+print(f"The answer to part 2 is: {answer_2}")
